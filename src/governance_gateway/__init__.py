276f8bd99ff1af215c3ed3c390deba0fd5a86514
wking53214 <wking53214@gmail.com>
2026-08-23 19:33:19 -0400

    Add Governance Gateway v0.1 red-team baseline

---
+++ src/governance_gateway/__init__.py
@@ -0,0 +1,19 @@
+from .gateway import GovernanceGateway
+from .models import (
+    Artifact,
+    Authority,
+    EpistemicStatus,
+    GateReason,
+    GateResult,
+    Scope,
+)
+
+__all__ = [
+    "Artifact",
+    "Authority",
+    "EpistemicStatus",
+    "GateReason",
+    "GateResult",
+    "GovernanceGateway",
+    "Scope",
+]
